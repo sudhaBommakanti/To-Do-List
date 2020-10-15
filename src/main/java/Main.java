842e5a1d1e239ef66de6d3a5a78b6f62bdd61a6c
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-10-15 20:44:13 +0200

    added comments to the main file and added show list method and few implementations for the edit task method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,38 +6,58 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
+/**
+ * Main class to implement all the valid command prompt options for the application
+ * along with menu options for the user.
+ *
+ * @author  Sudha Bommakanti
+ * @version 2020.10.11
+ */
 public class Main {
 
     private FileHandler fileHandler;
-    private String path = "/Users/sudhabommakanti/IdeaProjects/To-Do-List/src/";
+    private String path = "/Users/sudhabommakanti/IdeaProjects/To-Do-List/src/main/java/";
     SimpleDateFormat format=new SimpleDateFormat("dd/MM/yyyy");
-    private static ArrayList<Task> taskList = new ArrayList<>();
+    //Public ArrayList<Task> getData = new ArrayList<>();
+    private ArrayList<Task> taskList = new ArrayList<>();
     public Task task;
 
+    /**
+     * Main class constructor which initialises the file handler and calls the filehandler read method
+     * to get the data to the user implement all the valid command prompt options for the application.
+     */
     public Main() {
         fileHandler = new FileHandler();
+        taskList = fileHandler.readAsData();
     }
 
+    /**
+     * text user interface - toDoList method which is used to print the welcome message to the user.
+     */
     public void toDoLists() throws ParseException{
         welcomeMessage();
     }
 
-    public int menu(){
-        int choice;
-        Scanner sc = new Scanner(System.in);
-        System.out.println(">> (1) Show Task List (by date or project) ");
-        System.out.println(">> (2) Add New Task ");
-        System.out.println(">> (3) Edit Task (update, mark as done, remove) ");
-        System.out.println(">> (4) Save and Quit");
-        System.out.print("> ");
-        choice = sc.nextInt();
-        return choice;
+    /**
+     * Private method welcomeMessage()
+     * This method calls the user interface options for the - toDoList.
+     */
+    private void welcomeMessage() throws ParseException{
+        System.out.println(">> Welcome to ToDoLy");
+        System.out.println(">> You have 0 tasks todo and 0 tasks are done!");
+        System.out.println(">> Pick an option:");
+        showCommands();
     }
 
+    /**
+     * Private method showCommands()
+     * With this method the user can enter his choice of operation,redirected to the corresponding
+     * option method and can come back to main menu.
+     */
     private void showCommands() throws ParseException {
         int menuItem = -1;
         while( menuItem != 0){
-           menuItem = menu();
+            menuItem = menu();
             switch (menuItem){
                 case 1:
                     showList();
@@ -51,29 +71,54 @@ public class Main {
                     System.out.println("Edit a new task");
                     break;
                 case 4:
-                    System.out.println("Save the task and continue");
                     saveAndQuit();
                     break;
                 default:
-                    System.out.println("Invalid Input");
+                    System.out.println("Not a valid input");
             }
         }
-
     }
 
-    private void welcomeMessage() throws ParseException{
-        System.out.println(">> Welcome to ToDoLy");
-        System.out.println(">> You have 0 tasks todo and 0 tasks are done!");
-        System.out.println(">> Pick an option:");
-        showCommands();
+    /**
+     * public method menu()
+     * This method gives the user options to show Task list, add a new task, show task,
+     * edit a task,save the task and continue to the main menu.
+     * returns user choice as int.
+     */
+    public int menu(){
+        int choice;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(">> (1) Show Task List (by date or project) ");
+        System.out.println(">> (2) Add New Task ");
+        System.out.println(">> (3) Edit Task (update, mark as done, remove) ");
+        System.out.println(">> (4) Save and Quit");
+        System.out.print("> ");
+        choice = sc.nextInt();
+        return choice;
     }
 
-    public void editTask() {
-        System.out.println("edit the tasks");
+    /**
+     * public method showList()
+     * This method show the list of tasks that the user has saved to the task list file.
+     */
+    public void showList() {
+        System.out.println("\nTask List\n");
+        //ArrayList<Task> checkData = fileHandler.readAsData();
+       // System.out.println(checkData);
+        int number = 0;
+        for (Task item : taskList) {
+            System.out.println(++number + ". " + item.getItem());
+        }
     }
 
+    /**
+     * public method addTask()
+     * This method allows the user to add a new task with the use of Task model class
+     * with a task title, due date, status and project.
+     * Then stores the Task to the file.
+     * Can continue with the left task.
+     */
     public void addTask() throws ParseException {
-
         final int MAX = 10;
         int count = 0;
         Scanner input = new Scanner(System.in);
@@ -82,7 +127,6 @@ public class Main {
         System.out.println("Type 'yes' to add task to your list else type 'stop'.");
 
         for ( int i = 0; i < MAX; i++ ) {
-
             userEnteredItem = input.nextLine();
             if(!userEnteredItem.isEmpty()) {
                 list[i] = userEnteredItem;
@@ -90,7 +134,6 @@ public class Main {
                 if (userEnteredItem.equals("stop"))  {
                     break;
                 }
-
                 System.out.print("Enter the task title:");
                 Scanner scanner = new Scanner(System.in);
                 String title = scanner.nextLine();
@@ -119,43 +162,86 @@ public class Main {
                 i--; // Do not increase index for empty item.
             }
         }
+    }
+
+    public int editMenu(){
+        int choice;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("(1) Update the Task ");
+        System.out.println("(2) Mark as Done");
+        System.out.println("(3) Remove the Task");
+        System.out.print("> ");
+        choice = sc.nextInt();
+        return choice;
+    }
+
+    public void editTask(){
+        int menuItem = -1;
+        while( menuItem != 0){
+            menuItem = editMenu();
+            switch (menuItem){
+                case 1:
+                    updateTask(task);
+                    break;
+                case 2:
+                    markAsDone();
+                    break;
+                case 3:
+                    remove();
+                    System.out.println("Edit a new task");
+                    break;
+                default:
+                    System.out.println("Invalid Input");
+            }
+        }
 
     }
 
-    public void saveAndQuit() {
-        System.exit(0);
+    //method to be implemented.
+    public void updateTask(Task task) {
+        System.out.println("update the task");
+        //ArrayList<Task> checkData = fileHandler.readAsData();
+        System.out.println(taskList);
     }
 
-    public void showList() {
-        System.out.println("\nTask List\n");
-           try {
-               Scanner inFile = new Scanner(new FileReader( path + "Task.txt"));
-               String line;
-               int number = 1;
-               while(inFile.hasNextLine()){
-                   line = inFile.nextLine();
-                   System.out.println(number + ". " + line);
-                   ++number;
-               }
-               System.out.println();
-               inFile.close();
-           } catch (FileNotFoundException ioe) {
-               System.out.println(ioe);
-           }
+    public void markAsDone() {
+        System.out.println("mark as done"); // method to be implemented
+    }
 
-        ArrayList<Task> checkData = fileHandler.readAsData();
-        System.out.println(checkData);
+    /**
+     * sub task of edit task method
+     * removes the task from the list of tasks
+     * gets the index number of the task and removes the task with that index number
+     * updates the file after a task is removed.
+     */
+    public void remove() {
+        System.out.println("remove a task");
         System.out.println("----------------------");
-        System.out.println("----------------------------------------------");
-        System.out.println("To-Do List");
-        System.out.println("----------------------------------------------");
-        int number = 0;
-        for (Task item : taskList) {
-            System.out.println(++number + ". " + item.getItem());
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What do you want to remove? (Enter number): ");
+        int index = scanner.nextInt();
+        if((index-1)<0 || index>taskList.size()) {
+            System.out.println("Wrong index number! Please enter in range of 1 to "+taskList.size());
+        } else {
+            taskList.remove(index-1);
+            fileHandler.writeAsData(taskList);
         }
         System.out.println("----------------------");
+        System.out.println("Task Removed!");
     }
 
+    /**
+     * method to save the task to file and quit from the file to come back
+     * to main menu options.
+     */
+    public void saveAndQuit() {
+        System.out.println("save the task and continue"); // under implementation.
+    }
+
+    /**
+     * main method to start the user command line interface
+     * creates Main class object and calls toDoLists method.
+     */
     public static void main(String[] args) throws ParseException {
         Main main = new Main();
         main.toDoLists();
