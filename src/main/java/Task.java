7bc1ec99873bd4ba88a2f05942112df3b078d784
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-10-20 16:46:50 +0200

    added setters methods to Task class, added override toString method and override compareTo method

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -5,18 +5,18 @@ import java.io.Serializable;
 
 /**
  * Model class for Task
- * contains getters for the task fields
- * @author  Sudha Bommakanti
+ * contains getters and setters for Task class fields
+ *
+ * @author Sudha Bommakanti
  * @version 2020.10.11
  */
-public class Task implements Serializable
-{
+public class Task implements Serializable, Comparable<Task> {
     private String title;
     private Date dueDate;
     private String status;
     private String project;
 
-    SimpleDateFormat format=new SimpleDateFormat("dd/MM/yyyy");
+    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 
     //Task constructor which initializes the task fields when the task object is created.
     public Task(String title, Date dueDate, String status, String project) {
@@ -26,36 +26,57 @@ public class Task implements Serializable
         this.project = project;
     }
 
-    //getTitle method to get the Task title
+    //getTitle method to get Task title
     public String getTitle() {
         return this.title;
     }
 
-    //getDate method to get the Task due date
+    //getDate method to get Task due date
     public Date getDate() {
         return this.dueDate;
     }
 
-    //getStatus method to get the Task status
+    //getStatus method to get Task status
     public String getStatus() {
         return this.status;
     }
 
-    //getProjectCategory method to get the Task category(Home,Work etc;)
+    //getProjectCategory method to get Task category(Home,Work etc;)
     public String getProjectCategory() {
         return this.project;
     }
 
-    //getItem method to get the Task details
-    public String getItem() {
-        return "" + this.title + "**" + format.format(this.dueDate) + "**" + this.status + "**" + this.project;
-        //return "" + this.title + "**" + format.format(this.dueDate) + "**" + this.status + "**" + this.project ;
+    //setTitle method to set the Task title
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    //setDate method to set Task due date
+    public void setDate(Date date) {
+        this.dueDate = date;
+    }
+
+    //setStatus method to set Task status(todo, done)
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    //setProjectCategory method to set Task category(Home,Work)
+    public void setProjectCategory(String project) {
+        this.project = project;
     }
 
     @Override
     public String toString() {
         return "" + this.title + "**" + format.format(this.dueDate) + "**" + this.status + "**" + this.project;
-        //return "" + this.title + "**" + format.format(this.dueDate) + "**" + this.status + "**" + this.project ;
+    }
+
+    @Override
+    public int compareTo(Task task) {
+        if (getDate() == null || task.getDate() == null) {
+            return 0;
+        }
+        return getDate().compareTo(task.getDate());
     }
 
 }
